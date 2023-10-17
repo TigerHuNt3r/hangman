8827f295632feab99dd8d0338ae278e0a30046fa
diak <diak@mesterlevel.lo>
2023-10-17 11:56:58 +0200

    controller bövitées

--- lib/controllers/game_view_controller.dart
+++ lib/controllers/game_view_controller.dart
@@ -1,4 +1,56 @@
 // játék logika
+import 'dart:math';
+import 'package:hangman/models/tipp.dart';
 import 'package:get/get.dart';
 
-class GameViewController extends GetxController {}
+class GameViewController extends GetxController {
+  List<String> szovegek = [
+    "Psycho",
+    "Intellect",
+    "Autism",
+    "Schizophrenia",
+    "Mental Stability",
+    "Intelligence",
+    "Full Scale IQ",
+    "Battle IQ",
+    "Emotional Intelligence",
+    "Strategy",
+    "Intelligent Psycho",
+    "Knowledge",
+    "Planning",
+    "Manipulation",
+    "Deception",
+    "Mental Strength",
+    "Mental Ability",
+    "Smart",
+    "Empathy",
+    "Logical Thinking",
+    "Creative Thinking",
+    "Fast Thinking",
+
+  ];
+  String randomSzoveg = "";
+  String csillagosSzoveg = "";
+  
+  void randomSzovegGeneralasa() {
+    int index = Random().nextInt(szovegek.length);
+    randomSzoveg = szovegek[index];
+    csillagosSzoveg="";
+    for(int i=0;i < randomSzoveg.length;i++){
+      csillagosSzoveg += "*";
+    }
+    update();
+  }
+
+  List<Tipp> tippek = [];
+  
+  bool nyertEaFelhasznalo(){
+    return csillagosSzoveg==randomSzoveg;
+  }
+
+  @override
+  void onInit() {
+    super.onInit();
+    randomSzovegGeneralasa();
+  }
+}
