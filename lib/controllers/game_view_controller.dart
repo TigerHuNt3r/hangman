083a16daf69d7a23577dd772df8d112abd8acac3
diak <diak@mesterlevel.lo>
2023-10-24 14:00:51 +0200

    2023.10.24

--- lib/controllers/game_view_controller.dart
+++ lib/controllers/game_view_controller.dart
@@ -2,6 +2,7 @@
 import 'dart:math';
 import 'package:hangman/models/tipp.dart';
 import 'package:get/get.dart';
+import 'package:hangman/views/end_view.dart';
 
 class GameViewController extends GetxController {
   List<String> szovegek = [
@@ -13,6 +14,7 @@ class GameViewController extends GetxController {
     "Intelligence",
     "Full Scale IQ",
     "Battle IQ",
+    "Overall EQ",
     "Emotional Intelligence",
     "Strategy",
     "Intelligent Psycho",
@@ -27,25 +29,114 @@ class GameViewController extends GetxController {
     "Logical Thinking",
     "Creative Thinking",
     "Fast Thinking",
-
+    "Academic Thinking",
+    "Genius",
+    "Creativity",
+    "Execution Control",
+    "Success Rate",
+    "Less Limitations",
+    "Cruel",
+    "Ruthless",
+    "Brutality",
+    "Pure Evil",
+    "Psychological Trick",
+    "Psychological Strength",
+    "Methods",
+    "Observation",
+    "Cold Reading",
+    "Detection",
+    "Charisma",
+    "Influence",
+    "Walter Hartwell White",
+    "Michael Scofield",
+    "Dexter Morgan",
+    "Dr. Hannibal Lecter",
+    "Patrick Jane",
+    "John Kramer",
+    "William Afton",
+    "Springtrap",
+    "Sherlock Holmes",
+    "Will Graham",
+    "Joker",
+    "Albert Wesker",
+    "Gustavo Fring",
   ];
   String randomSzoveg = "";
   String csillagosSzoveg = "";
-  
+
   void randomSzovegGeneralasa() {
     int index = Random().nextInt(szovegek.length);
     randomSzoveg = szovegek[index];
-    csillagosSzoveg="";
-    for(int i=0;i < randomSzoveg.length;i++){
+    csillagosSzoveg = "";
+    for (int i = 0; i < randomSzoveg.length; i++) {
       csillagosSzoveg += "*";
     }
     update();
   }
 
   List<Tipp> tippek = [];
+
+  bool nyertEaFelhasznalo() {
+    return csillagosSzoveg == randomSzoveg;
+  }
+
+  bool aMegadottBetuVoltEmar(String betu) {
+    bool tartalmazza = false;
+    for (int i = 0; i < tippek.length; i++) {
+      if (tippek[i].karakter == betu) {
+        tartalmazza = true;
+        break;
+      }
+    }
+    return tartalmazza;
+  }
+
+  bool vesztettEaFelhasznalo() {
+    int szamalao = 0;
+    for (int i = 0; i < tippek.length; i++) {
+      if (tippek[i].talaltE == false) {
+        szamalao++;
+      }
+    }
+    if (szamalao == 6)
+      return true;
+    else
+      return false;
+  }
   
-  bool nyertEaFelhasznalo(){
-    return csillagosSzoveg==randomSzoveg;
+
+  int hibakSzama() {
+    int szamalao = 0;
+    for (int i = 0; i < tippek.length; i++) {
+      if (tippek[i].talaltE == false) {
+        szamalao++;
+      }
+    }
+    return szamalao;
+  }
+
+  void tippHozzaadsa(String betu) {
+    if (randomSzoveg.toLowerCase().contains(betu.toLowerCase())) {
+      List<String> csillagosSzovegTemp = csillagosSzoveg.split('');
+      for (int i = 0; i < randomSzoveg.length; i++) {
+        if (randomSzoveg[i].toLowerCase() == betu.toLowerCase()) {
+          csillagosSzovegTemp[i] = randomSzoveg[i];
+        }
+      }
+      csillagosSzoveg = csillagosSzovegTemp.join();
+
+      tippek.add(Tipp(karakter: betu, talaltE: true));
+      update();
+      if(nyertEaFelhasznalo()){
+        Get.to(EndView("A PICSÁBA! Megmentettél egy cigányt!", hibakSzama()));
+      }
+    } else {
+      tippek.add(Tipp(karakter: betu, talaltE: false));
+      update();
+      if(vesztettEaFelhasznalo()){
+        Get.to(EndView("GRATULÁLUNK! Felakasztottál egy cigányt! Megoldás: ${randomSzoveg} ", hibakSzama()));
+      }
+    }
   }
 
   @override
--- lib/views/game_view.dart
+++ lib/views/game_view.dart
@@ -15,15 +15,48 @@ class GameView extends StatelessWidget {
             title: Text("Hangman"),
           ),
           body: Column(children: [
+            Image.asset("images/${controller.hibakSzama()}.png"),
             Text(controller.randomSzoveg),
             Text(controller.csillagosSzoveg),
-            CupertinoButton(child: 
-            Text("Generálj ujjat"), onPressed: () {
-              controller.randomSzovegGeneralasa();
-            },)
+            Column(children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                LetterButton(letter: "A", controller: controller),
+                LetterButton(letter: "B", controller: controller),
+                LetterButton(letter: "C", controller: controller),
+                LetterButton(letter: "D", controller: controller),
+                LetterButton(letter: "E", controller: controller),
+                LetterButton(letter: "F", controller: controller),
+              ],
+              )
+            ]
+            ,)
           ]),
         );
       }
+    );
+  }
+}
+
+class LetterButton extends StatelessWidget {
+  const LetterButton({
+    required this.letter,
+    required this.controller,
+  });
+
+  final GameViewController controller;
+  final String letter;
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoButton.filled(
+      padding: EdgeInsets.zero,
+      child: Text("A"),
+      onPressed: (!controller.aMegadottBetuVoltEmar("A"))
+      ? null
+      : () => controller.tippHozzaadsa("A"), 
+
     );
   }
 }
